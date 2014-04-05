4be2175129de1020739bbbaf42fd9b4468377abc
ande3577 <ande3577@gmail.com>
2014-04-05 07:55:34 -0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_customize_notification do
+  name 'Redmine Customize Notification plugin'
+  author 'David S Anderson'
+  description 'Allows a user to customize the issue fields for which to receive notification emails.'
+  version '0.0.1'
+  url 'https://github.com/ande3577/redmine_customize_notification'
+  author_url 'https://github.com/ande3577'
+end
