3129f8ca4dabf4004776a280e63919f5e7cffa73
ande3577 <ande3577@gmail.com>
2014-04-07 20:27:45 -0500

    Notification events now use the issue property id

--- assets/javascripts/customize_notification.js
+++ assets/javascripts/customize_notification.js
@@ -3,10 +3,10 @@ var disable_control = function(control, disabled) {
 };
 
 var draw = function() {
-  var issue_status_changed = $(enabled_notifications_issue_status_changed).is(':checked');
+  var issue_status_changed = $(enabled_notifications_status_id).is(':checked');
   disable_control($(enabled_notifications_issue_closed), issue_status_changed);
   disable_control($(enabled_notifications_issue_reopened), issue_status_changed);
-  var issue_assignee_changed = $(enabled_notifications_issue_assignee_changed).is(':checked');
+  var issue_assignee_changed = $(enabled_notifications_assigned_to_id).is(':checked');
   disable_control($(enabled_notifications_issue_assignee_to_me), issue_assignee_changed);
   disable_control($(enabled_notifications_issue_assignee_from_me), issue_assignee_changed);
 };
--- lib/notification_events.rb
+++ lib/notification_events.rb
@@ -1,22 +1,23 @@
 module NotificationEvents
   ISSUE_EVENTS = {
-    :issue_status_changed => :notification_event_status_changed,
+    :tracker_id => :notification_event_tracker_changed,
+    :status_id => :notification_event_status_changed,
     :issue_closed => :notification_event_issue_closed,
     :issue_reopened => :notification_event_issue_reopened,
-    :issue_subject_changed => :notification_event_subject_changed,
-    :issue_priority_changed => :notification_event_priority_changed,
-    :issue_assignee_changed => :notification_event_assignee_changed,
+    :category_id => :notification_event_category_changed,
+    :assigned_to_id => :notification_event_assignee_changed,
     :issue_assignee_to_me => :notification_event_assignee_changed_to_me,
     :issue_assignee_from_me => :notification_event_assignee_changed_from_me,
-    :issue_category_changed => :notification_event_category_changed,
-    :issue_target_version_changed => :notification_event_target_version_changed,
-    :issue_parent_issue_changed => :notification_event_parent_issue_changed,
-    :issue_start_date_changed => :notification_event_start_date_changed,
-    :issue_due_date_changed => :notification_event_due_date_changed,
-    :issue_estimated_time_changed => :notification_event_estimated_time_changed,
-    :issue_percent_done_changed => :notification_event_percent_done_changed,
-    :issue_description_changed => :notification_event_description_changed,
-    :issue_commented_on => :notification_event_issue_commented_on
+    :priority_id => :notification_event_priority_changed,
+    :fixed_version_id => :notification_event_target_version_changed,
+    :subject => :notification_event_subject_changed,
+    :description => :notification_event_description_changed,
+    :start_date => :notification_event_start_date_changed,
+    :due_date => :notification_event_due_date_changed,
+    :done_ratio => :notification_event_percent_done_changed,
+    :estimated_hours => :notification_event_estimated_time_changed,
+    :parent_issue_id => :notification_event_parent_issue_changed,
+    :notes => :notification_event_issue_commented_on
   }.freeze
   
   def dependent_event?(event)
